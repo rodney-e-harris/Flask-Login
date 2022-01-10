2878d3549c436b1d89774f81f806c6bc34258112
rodneyharris <rodneyeharrisjr@gmail.com>
2022-01-10 08:00:59 -0600

    Updated the testscript for /login POST requests

--- Backend/testscript.py
+++ Backend/testscript.py
@@ -2,15 +2,12 @@ import pytest
 import flask
 import requests
 
+#This testscrip can be used to test POST requests to the /login route
 url = 'http://127.0.0.1:5000/login'
-userdata = {'username': "admin", 'password': "pass"}
-x = requests.post(url, data = userdata)
-
-
-if(x.text == "1"):
-    print("Success")
-else:
-    print("Failure")
+userdata = {'username': "asdf", 'password': "asdf"}
+x = requests.post(url, json = userdata)
 
 print(x.text)
 
+x = requests.get('http://127.0.0.1:5000/users/asdf')
+print(x.text)
